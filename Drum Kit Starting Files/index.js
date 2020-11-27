3dbf98ceb4ee2551b1fecd30eda360021a6056c6
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-28 02:43:00 +0900

    edit: using for-loop

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,4 +1,4 @@
-const numberOfDrumButtons = document.querySelectorAll('.drum').length
+const numberOfDrumButtons = document.querySelectorAll('.drum').length;
 
 const crash = new Audio('./sounds/crash.mp3');
 const kick = new Audio('./sounds/kick-bass.mp3');
@@ -9,28 +9,34 @@ const tom3 = new Audio('./sounds/tom-3.mp3');
 const tom4 = new Audio('./sounds/tom-4.mp3');
 
 
-document.querySelector('.w').addEventListener('click', () => tom1.play());
-document.querySelector('.a').addEventListener('click', () => tom2.play());
-document.querySelector('.s').addEventListener('click', () => tom3.play());
-document.querySelector('.d').addEventListener('click', () => tom4.play());
-document.querySelector('.j').addEventListener('click', () => crash.play());
-document.querySelector('.k').addEventListener('click', () => kick.play());
-document.querySelector('.l').addEventListener('click', () => snare.play());
+// document.querySelector('.w').addEventListener('click', () => tom1.play());
+// document.querySelector('.a').addEventListener('click', () => tom2.play());
+// document.querySelector('.s').addEventListener('click', () => tom3.play());
+// document.querySelector('.d').addEventListener('click', () => tom4.play());
+// document.querySelector('.j').addEventListener('click', () => crash.play());
+// document.querySelector('.k').addEventListener('click', () => kick.play());
+// document.querySelector('.l').addEventListener('click', () => snare.play());
+
+// document.addEventListener('keydown', (event) => playSound(event.key) );
+
+// 위의 코드들을 for loop으로 대체하여 깔끔하게 만들기
+for (let i = 0; i < numberOfDrumButtons ; i++){
+  
+  document.querySelectorAll('.drum')[i].addEventListener('keypress', function(event) {
+    playSound(event.key);
+    buttonAnimation(event.key);
+  });
+
+  document.querySelectorAll('.drum')[i].addEventListener('click', function() {
+    let ButtonInnerHTML = this.innerHTML;
+
+    playSound(ButtonInnerHTML);
+    buttonAnimation(ButtonInnerHTML);
+  });
+
+};
 
 
-
-// document.addEventListener('keydown', () => {
-//   let buttonInnerHTML = this.innerHTML;
-// });
-
-
-for (i = 0 ; i < numberOfDrumButtons; i++){
-  document.querySelectorAll('button')[i].addEventListener('click', 
-  function(){this.style.color = 'white'})
-}
-
-
-document.addEventListener('keydown', (event) => playSound(event.key) );
 
 function playSound(key){
   switch (key) {
@@ -62,8 +68,15 @@ function playSound(key){
       snare.play()
       break;
 
-
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey){
+  // document.querySelector('.'+currentKey).classList.toggle('pressed');
+
+  let activeButton = document.querySelector(`.${currentKey}`);
+  activeButton.classList.add('pressed');
+  setTimeout( () => activeButton.classList.remove('pressed'), 100);
 }
